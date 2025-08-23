8c0a43466e1f15abb43192ad9b03133a949e06d9
money money <gamermanishgame@gmail.com>
2025-08-24 00:27:59 +0530

    count

---
+++ Day-10-count-foreach/count/main.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "name" {
+    ami = "ami-08a6efd148b1f7504"
+    instance_type = "t2.micro"
+    count = length(var.ec2)
+    tags = {
+        #get count index value
+      Name = var.ec2[count.index]
+    }
+  
+}
+
+variable "ec2" {
+    type = list(string)
+    default = [ "dev", "test" , "prod", "preprod" ]
+  
+}
